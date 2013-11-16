9cd59052b495a47728526798f6573b2d1df85ac6
Ayoze <Ayoze@AyoVera-PC>
2013-11-16 02:12:04 +0000

    Model package (not implemented currencyset, moneyexchanger, number)

--- src/Model/ExchangeRate.java
+++ src/Model/ExchangeRate.java
@@ -1,7 +1,24 @@
-
 package Model;
 
+import java.util.Date;
 
 public class ExchangeRate {
+    Date date;
+    Currency from;
+    Currency to;
+    Number rate;
 
+    public ExchangeRate(Currency from, Currency to, Number rate) {
+        this.from = from;
+        this.to = to;
+        this.rate = rate;
+    }
+
+    public ExchangeRate(Date date, Currency from, Currency to, Number rate) {
+        this.date = date;
+        this.from = from;
+        this.to = to;
+        this.rate = rate;
+    }
+    
 }
