d8349b0d056affee20b123f13a68e8329f26bea8
Ayoze <Ayoze@AyoVera-PC>
2013-11-21 15:26:10 +0000

    All packages created, just lets implement!

---
+++ src/UserInterface/MoneyDialog.java
@@ -0,0 +1,15 @@
+
+package UserInterface;
+
+import Model.Money;
+
+
+public class MoneyDialog {
+    Money money;
+    
+    private void execute(){
+
+    }
+}
+
+
---
+++ src/UserInterface/MoneyViewer.java
@@ -0,0 +1,14 @@
+
+package UserInterface;
+
+import Model.Money;
+
+
+public class MoneyViewer {
+    Money money;
+    
+    public void refresh(){
+    
+    }
+        
+}
